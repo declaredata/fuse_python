815686ab8bcf2ec0cf65f1a007aa1a9c69d017d3
Aaron Schlesinger <aaron@ecomaz.net>
2024-12-18 22:34:29 -0800

    progress

--- declaredata_fuse/src/declaredata_fuse/proto/sds_pb2_grpc.py
+++ declaredata_fuse/src/declaredata_fuse/proto/sds_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from proto import sds_pb2 as proto_dot_sds__pb2
+from declaredata_fuse.proto import sds_pb2 as proto_dot_sds__pb2
 
 
 class sdsStub(object):
