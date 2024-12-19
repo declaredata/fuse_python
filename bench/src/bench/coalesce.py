ad02bc6626d5069afab60fa0da06957553417289
Aaron Schlesinger <aaron@ecomaz.net>
2024-12-19 12:58:18 -0800

    small change

--- bench/src/bench/coalesce.py
+++ bench/src/bench/coalesce.py
@@ -3,5 +3,8 @@ import declaredata_fuse.functions as F
 
 
 def coalesce_basic(df: DataFrame) -> DataFrame:
-    df2 = df.select(df.year, F.coalesce(df.state_abbr, df.state_name))
+    df2 = df.select(
+        df.year,
+        F.coalesce(df.state_abbr, df.state_name),
+    )
     return df2
