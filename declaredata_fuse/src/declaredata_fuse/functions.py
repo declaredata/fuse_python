a99f17934f5c21ce0703b17b964b88c819c040a2
Aaron Schlesinger <aaron@ecomaz.net>
2024-12-18 17:32:36 -0800

    more fixes

--- declaredata_fuse/src/declaredata_fuse/functions.py
+++ declaredata_fuse/src/declaredata_fuse/functions.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from declaredata_fuse.column import Column, SortDirection, SortedColumn
-from declaredata_fuse.column_filter import FilterColumn, FilterFunction
 from declaredata_fuse.column_or_name import ColumnOrName, col_or_name_to_basic
 from declaredata_fuse.proto.sds_pb2 import (
     AggOperation,
@@ -57,10 +56,6 @@ def last(col_name: str) -> "Function":
     """Create a function to find the last value"""
     return Function(col_name=col_name, op=AggOperation.LAST)
 
-
-def filter(col: Column | str, func: FilterFunction) -> Column:
-    col_reified = col.cur_name() if isinstance(col, Column) else col
-    return FilterColumn(col_name=col_reified, func=func)
 
 @dataclass(frozen=True)
 class Function:
