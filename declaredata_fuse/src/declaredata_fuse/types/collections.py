9ad796dadd999e9aed3a4534d42ba11730caed13
Aaron Schlesinger <aaron@ecomaz.net>
2025-01-12 11:37:40 -0800

    removing top-level types module
    in favor of the one in `sql.types`

--- declaredata_fuse/src/declaredata_fuse/types/collections.py
+++
@@ -1,5 +0,0 @@
-from declaredata_fuse.types.data_type import DataType
-
-
-class MapType(DataType):
-    pass
--- declaredata_fuse/src/declaredata_fuse/types/string.py
+++
@@ -1,5 +0,0 @@
-from declaredata_fuse.types.data_type import DataType
-
-
-class StringType(DataType):
-    pass
